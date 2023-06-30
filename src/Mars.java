6c48bdbd0639f8e00e21ac1608e78a09b82b5d87
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-06-29 23:26:23 -0700

    Initial commit

---
+++ src/Mars.java
@@ -0,0 +1,8 @@
+public class Mars {
+    public static void main(String[] args) {
+        String colonyName = "SpaceX";
+        int shipPopulation = 300;
+        double meals = 4000.00;
+        boolean landing = true;
+    }
+}
